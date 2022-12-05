819a591855eea4cd314d43b6c5a43edd5e95b922
haykct <https://github.com/HaykoHayrapetyan>
2022-12-05 17:03:29 +0100

    Created project.

---
+++ CarRemote/CarRemoteApp/CarRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarRemoteApp.swift
+//  CarRemote
+//
+//  Created by Hayk Hayrapetyan on 05.12.22.
+//
+
+import SwiftUI
+
+@main
+struct CarRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
