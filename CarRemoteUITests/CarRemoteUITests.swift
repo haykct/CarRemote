b6873fdc2cc716adcbeadc4e1b1fdd07231747a8
haykct <https://github.com/HaykoHayrapetyan>
2022-12-09 00:27:44 +0100

    Added ui test.

---
+++ CarRemoteUITests/CarRemoteUITests.swift
@@ -0,0 +1,31 @@
+//
+//  CarRemoteUITests.swift
+//  CarRemoteUITests
+//
+//  Created by Hayk Hayrapetyan on 08.12.22.
+//
+
+import XCTest
+import SwiftUI
+
+final class CarRemoteUITests: XCTestCase {
+    
+    func testUIUpdateAfterDoorLock() {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let lock = app.buttons["lock"]
+        XCTAssert(lock.exists)
+        lock.tap()
+        
+        let alertYes = app.alerts["Are you sure?"].buttons["Yes, Lock"]
+        XCTAssert(alertYes.exists)
+        
+        let alertCancel = app.alerts["Are you sure?"].buttons["Cancel"]
+        XCTAssert(alertCancel.exists)
+        alertYes.tap()
+        XCTAssert(!app.buttons["lock"].exists)
+        XCTAssert(app.staticTexts["..."].exists)
+        XCTAssert(app.buttons["lock"].waitForExistence(timeout: 6))
+    }
+}
---
+++ CarRemoteUITests/CarRemoteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CarRemoteUITestsLaunchTests.swift
+//  CarRemoteUITests
+//
+//  Created by Hayk Hayrapetyan on 08.12.22.
+//
+
+import XCTest
+
+final class CarRemoteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
